dee76812d5314775aa5d17d6e4c8ffbff2421016
arkar <rluputkyut@gmail.com>
2022-02-06 11:17:27 +0800

    Initial check-in of module BeautyCollection

---
+++ BeautyCollection/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using BeautyCollection.ViewModels;
+using BeautyCollection.Views;
+using Xamarin.Forms;
+
+namespace BeautyCollection
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ BeautyCollection/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using BeautyCollection.ViewModels;
+
+namespace BeautyCollection.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
